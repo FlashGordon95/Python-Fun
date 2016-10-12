f3a99b30dc3bd60256469b78b641ab42ba9956dc
FlashGordon95 <ryangordon210@gmail.com>
2016-10-12 13:25:40 +0100

    Experimenting with requests
    Tried to get request working using poloniex api.
    Trying to make a route for different currencies.
    Experimenting with json.dumps

--- httpRequestsRelay/poloniex.py
+++ httpRequestsRelay/poloniex.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import json
 import requests
 
 app = Flask(__name__)
@@ -9,8 +10,40 @@ app = Flask(__name__)
 def hello_world():
     r = requests.get('https://poloniex.com/public?command=returnTicker')
     print(r)
-    return r.text.BTC_1CR
+    data = r.json()
+    test = [{key} for key in data.keys()]
 
+    # print("The response is" +str(data['BTC_SDC']))
+    print(data['BTC_SDC'])
+
+    return "Hello World"
 # Run our server everywhere. Can be accessed via the IP of the host computer on port 5000
 
-app.run(host='0.0.0.0')
+@app.route('/sdc')
+def BTC_SDC():
+    r = requests.get('https://poloniex.com/public?command=returnTicker')
+    print(r)
+    data = r.json()
+    test = [{key} for key in data.keys()]
+    
+    # print("The response is" +str(data['BTC_SDC']))
+    print(data['BTC_SDC'])
+    
+    
+    return json.dumps(data['BTC_SDC'])
+# Run our server everywhere. Can be accessed via the IP of the host computer on port 5000
+
+@app.route('/xmr')
+def BTC_XMR():
+    r = requests.get('https://poloniex.com/public?command=returnTicker')
+    print(r)
+    data = r.json()
+    test = [{key} for key in data.keys()]
+    print(test)
+    
+    # print("The response is" +str(data['BTC_SDC']))
+    print(data['BTC_XMR'])
+    
+    return data['BTC_XMR']
+
+app.run(debug=False)
