2427eb897fa511eb0cf463fb8f248945fb606e6d
FlashGordon95 <ryangordon210@gmail.com>
2016-10-12 14:24:33 +0100

    SDC and XMR
    Added routes for SDC and XMR trading pairs.
    Displaying lowestAsk, highestBid and avg of the two via the console.

--- httpRequestsRelay/poloniex.py
+++ httpRequestsRelay/poloniex.py
@@ -24,11 +24,27 @@ def BTC_SDC():
     r = requests.get('https://poloniex.com/public?command=returnTicker')
     print(r)
     data = r.json()
-    test = [{key} for key in data.keys()]
+    for key in data.keys():
+        print(key)
     
     # print("The response is" +str(data['BTC_SDC']))
-    print(data['BTC_SDC'])
+    print(data['BTC_SDC']['highestBid'])
     
+    
+    # In this section highBid and lowAsk are unicode
+    highBid  =  data['BTC_SDC']['highestBid']
+    lowAsk = data['BTC_SDC']['lowestAsk'];
+    
+    # In order to sum the values and not all the values for the list we need to convert the vars to floats
+    # Float allows us to add the decimal numbers together with precision
+    avgBid = (float(highBid) + float(lowAsk)) / 2
+    
+    # We could just print avgBid but to have a string also we will need to convert to str
+    print("This is the btc sdc highest bid "+ highBid)
+    print("This is the btc sdc lowest bid "+ lowAsk)
+    print("This is the btc sdc avg bid "+ str(avgBid))
+
+
     
     return json.dumps(data['BTC_SDC'])
 # Run our server everywhere. Can be accessed via the IP of the host computer on port 5000
@@ -38,12 +54,27 @@ def BTC_XMR():
     r = requests.get('https://poloniex.com/public?command=returnTicker')
     print(r)
     data = r.json()
-    test = [{key} for key in data.keys()]
-    print(test)
+    for key in data.keys():
+        print(key)
     
     # print("The response is" +str(data['BTC_SDC']))
-    print(data['BTC_XMR'])
+    print(data['BTC_XMR']['highestBid'])
     
-    return data['BTC_XMR']
+    
+    # In this section highBid and lowAsk are unicode
+    highBid  =  data['BTC_XMR']['highestBid']
+    lowAsk = data['BTC_XMR']['lowestAsk'];
+    
+    # In order to sum the values and not all the values for the list we need to convert the vars to floats
+    # Float allows us to add the decimal numbers together with precision
+    avgBid = (float(highBid) + float(lowAsk)) / 2
+    
+    # We could just print avgBid but to have a string also we will need to convert to str
+    print("This is the btc sdc highest bid "+ highBid)
+    print("This is the btc sdc lowest bid "+ lowAsk)
+    print("This is the btc sdc avg bid "+ str(avgBid))
+
+    
+    return json.dumps(data['BTC_XMR'])
 
 app.run(debug=False)
